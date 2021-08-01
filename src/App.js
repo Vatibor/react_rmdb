00ea806f7afb9db2a4558da7ea69f9dd1f9bbd05
Vatibor <vatibor2@gmail.com>
2021-08-01 17:04:26 +0200

    deploy on netlify, create rate component and logic, create login page

--- src/App.js
+++ src/App.js
@@ -8,20 +8,27 @@ import Header from './components/Header';
 import Home from './components/Home';
 import Movie from './components/Movie';
 import NotFound from './components/NotFound';
+import Login from './components/Login';
 
 // Styles
 import { GlobalStyle } from './GlobalStyle';
 
+// Context
+import UserProvider from './context';
+
 const App = () => (
     <Router>
+      <UserProvider>
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
         <Route path='/:movieId' element={<Movie />}/>
         <Route path='/*' element={<NotFound />}/>
       </Routes>
     
       <GlobalStyle />
+      </UserProvider>
     </Router>
   );
 
--- src/components/MovieInfo/index.js
+++ src/components/MovieInfo/index.js
@@ -1,54 +1,75 @@
-import React from 'react';
+import React, { useContext } from "react";
+import API from '../../API';
 
 // Component
-import Thumb from '../Thumb';
+import Thumb from "../Thumb";
+import Rate from "../Rate";
 
 // Config
-import { IMAGE_BASE_URL, POSTER_SIZE } from '../../config';
-
+import { IMAGE_BASE_URL, POSTER_SIZE } from "../../config";
 
 // Image
-import NoImage from '../../images/no_image.jpg';
-import PropTypes from 'prop-types';
+import NoImage from "../../images/no_image.jpg";
+import PropTypes from "prop-types";
 
 // Styles
-import { Wrapper, Content, Text } from './MovieInfo.styles';
+import { Wrapper, Content, Text } from "./MovieInfo.styles";
 
-const MovieInfo = ({ movie }) => (
-    <Wrapper backdrop={movie.backdrop_path}>
-        <Content>
-            <Thumb
-                image={
-                    movie.poster_path 
-                    ? `${IMAGE_BASE_URL}${POSTER_SIZE}${movie.poster_path}`
-                    : NoImage
+// Context
+import { Context } from "../../context";
+
+const MovieInfo = ({ movie }) => {
+    const [user] = useContext(Context);
+
+    const handleRating = async value => {
+        const rate = await API.rateMovie(user.sessionId, movie.id, value);
+        console.log(rate);
+    }
+
+    return (
+        <Wrapper backdrop={movie.backdrop_path}>
+            <Content>
+                <Thumb
+                    image={
+                        movie.poster_path
+                            ? `${IMAGE_BASE_URL}${POSTER_SIZE}${movie.poster_path}`
+                            : NoImage
                     }
                     clickable={false}
-            />
-            <Text>
-                <h1>{movie.title}</h1>
-                <h3>PLOT</h3>
-                <p>{movie.overview}</p>
+                />
+                <Text>
+                    <h1>{movie.title}</h1>
+                    <h3>PLOT</h3>
+                    <p>{movie.overview}</p>
 
-                <div className="rating-directors">
+                    <div className="rating-directors">
+                        <div>
+                            <h3>RATING</h3>
+                            <div className="score">{movie.vote_average}</div>
+                        </div>
+                        <div className="director">
+                            <h3>
+                                DIRECTOR{movie.directors.length > 1 ? "S" : ""}
+                            </h3>
+                            {movie.directors.map((director) => (
+                                <p key={director.credit_id}>{director.name}</p>
+                            ))}
+                        </div>
+                    </div>
+                    {user && (
                     <div>
-                        <h3>RATING</h3>
-                        <div className="score">{movie.vote_average}</div>
+                        <p>Rate Movie</p>
+                        <Rate callback={handleRating} />
                     </div>
-                    <div className="director">
-                        <h3>DIRECTOR{ movie.directors.length > 1 ? 'S' : ''}</h3>
-                        {movie.directors.map(director => (
-                            <p key={director.credit_id}>{director.name}</p>
-                        ))}
-                    </div>
-                </div>
-            </Text>
-        </Content>
-    </Wrapper>
-);
+                    )}
+                </Text>
+            </Content>
+        </Wrapper>
+    );
+};
 
 MovieInfo.propTypes = {
-    movie: PropTypes.object
+    movie: PropTypes.object,
 };
 
 export default MovieInfo;
